f0541a9c9e32b838064eef5d8caa63d3d0ca4769
mahdavifar2002 <mahdavifar2002@gmail.com>
2023-07-30 23:50:34 +0330

    Add initial script and readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # free-net
-For God's sake.
+For God's sake.  
+
+# Table of contents
+- [free-net](#free-net)
+- [Table of contents](#table-of-contents)
+- [On Europe Server](#on-europe-server)
+	- [Insall](#insall)
+	- [Restart](#restart)
+- [On Iran Server](#on-iran-server)
+	- [Insall](#insall-1)
+	- [Update](#update)
+	- [Restart](#restart-1)
+
+
+
+# On Europe Server
+
+## Insall
+Run the following command to install `x-ui` on Europe server. You will be prompted to enter a username, a password, and a panel port.
+```
+bash <(curl -Ls https://raw.githubusercontent.com/mahdavifar/free-net/main/script.sh) --europe --restart
+```
+
+
+## Restart
+In case if `x-ui` has stopped working, run the following command to restart it.
+```
+bash <(curl -Ls https://raw.githubusercontent.com/mahdavifar/free-net/main/script.sh) --europe --restart
+```
+
+# On Iran Server
+
+## Insall
+Run the following command command. Replace `1.1.1.1` with the IP of your Europe server.
+```
+bash <(curl -Ls https://raw.githubusercontent.com/mahdavifar/free-net/main/script.sh) --iran --install --server 1.1.1.1
+```
+
+## Update
+In case if you have changed IP of Europe server, run the following command command to update it. Replace `1.1.1.1` with the new IP of your Europe server.
+```
+bash <(curl -Ls https://raw.githubusercontent.com/mahdavifar/free-net/main/script.sh) --iran --update --server 1.1.1.1
+```
+
+## Restart
+After every reboot on Iran server, run the following command to activate packet forwarding.
+```
+bash <(curl -Ls https://raw.githubusercontent.com/mahdavifar/free-net/main/script.sh) --iran --restart
+```
---
+++ free-net.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -s|--server)
+      SERVER="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --iran)
+      IRAN=YES
+      shift # past argument
+      ;;
+    --europe)
+      EUROPE=YES
+      shift # past argument
+      ;;
+    -i|--install)
+      INSTALL=YES
+      shift # past argument
+      ;;
+    -u|--update)
+      UPDATE=YES
+      shift # past argument
+      ;;
+    -r|--restart)
+      RESTART=YES
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+
+
+# Iran server, responsible for iptables nat ###################################
+if [ "$IRAN" == "YES" ]; then
+
+	# Installing iptables -----------------------------------------------------
+	if [ "$INSTALL" == "YES" ]; then
+		echo "Installing on Iran server..."
+		sudo apt install -y netfilter-persistent
+		sudo apt install -y iptables-persistent
+		sudo ufw disable
+		sudo sysctl net.ipv4.ip_forward=1
+		sudo sh -c 'echo net.core.default_qdisc=fq >> /etc/sysctl.conf'
+		sudo sh -c 'echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf'
+		sudo sysctl -p
+		echo "Installation finished"
+	fi
+	
+	# Updating europe server ip address ---------------------------------------
+	if [ "$UPDATE" == "YES" ] || [ "$INSTALL" == "YES" ]; then
+		# reset -------------------------------------
+		sudo iptables -F
+		sudo iptables -X
+		sudo iptables -t nat -F
+		sudo iptables -t nat -X
+		sudo iptables -t mangle -F
+		sudo iptables -t mangle -X
+		sudo iptables -P INPUT ACCEPT
+		sudo iptables -P FORWARD ACCEPT
+		sudo iptables -P OUTPUT ACCEPT
+		sudo netfilter-persistent save
+		sudo netfilter-persistent reload
+		sudo service iptables restart
+		# accept ssh --------------------------------
+		sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j ACCEPT
+		# redirect other ports ----------------------
+		sudo iptables -t nat -A PREROUTING -p all -j DNAT --to-destination $SERVER
+		# post-routing ------------------------------
+		sudo iptables -t nat -A POSTROUTING ! -o lo -j MASQUERADE
+		# save and reload ---------------------------
+		sudo netfilter-persistent save
+		sudo netfilter-persistent reload
+		sudo service iptables restart
+	fi
+
+	# Restarting iptables -----------------------------------------------------
+	if [ "$RESTART" == "YES" ]; then
+		sudo sysctl net.ipv4.ip_forward=1
+		sudo service iptables restart
+	fi
+fi
+
+# Europe server, responsible for x-ui #########################################
+if [ "$EUROPE" == "YES" ]; then
+
+	# Installing x-ui ---------------------------------------------------------
+	if [ "$INSTALL" == "YES" ]; then
+		echo "Installing on Europe server..."
+		bash <(curl -Ls https://raw.githubusercontent.com/HexaSoftwareTech/x-ui/master/install.sh)
+		echo "Installation finished"
+	fi
+	
+	# Restarting x-ui ---------------------------------------------------------
+	if [ "$RESTART" == "YES" ]; then
+		x-ui restart
+	fi
+fi
+
